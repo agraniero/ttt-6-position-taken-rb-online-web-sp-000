fac8f23d0d7b2d71628dcec0ea78311e82f917c8
Anthony Graniero <ananfro@gmail.com>
2019-07-07 10:12:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken?(board, index)
   
-  if board[index] == "X" || "O"
-    return true
+  if 
   elsif board[index] == "" || " " || nil
-    return false
+    return falseboard[index] == "X" || "O"
+    return true
   end
 end
